89e2bdc6d544dc44d7a10c04d259a64e779cc062
Allan <workofcode@gmail.com>
2025-01-18 21:19:42 +0000

    - style home hero unit

--- Frontend/src/Views/Home.vue
+++ Frontend/src/Views/Home.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-      <h1>Home</h1>
-      <p>Welcome to a Page</p>
+  <div class="page-container">
+    <div class="hero-unit">
+      <h1>Welcome to Vuket</h1>
+      <p>Wow, what an amazing website this is. I bet you are super impressed. Admit it.</p>
+    </div>
   </div>
+
 </template>
 <script lang="ts">
 </script>
